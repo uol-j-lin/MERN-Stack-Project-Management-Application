fa22c4b7ca0013ecc4bb2e6d6b8d50b678eee74f
jjlin526 <jjlin526@gmail.com>
2022-09-25 22:01:51 -0500

    Create GraphQL schema with client type and queries

---
+++ server/schema/schema.js
@@ -0,0 +1,37 @@
+const { projects, clients } = require("../sampleData");
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+} = require("graphql");
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+// Root query object to fetch clients and projects
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => clients.id === args.id);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
