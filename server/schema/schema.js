df78924cd248cbb44a512df60cdaade0d058844b
jjlin526 <jjlin526@gmail.com>
2022-09-26 12:07:35 -0500

    Added queries to fetch project(s). Tested on GraphiQL

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,10 +19,34 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
+
 // Root query object to fetch clients and projects
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    client: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
