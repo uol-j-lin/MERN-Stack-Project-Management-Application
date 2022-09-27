4f5080ea5e30b7c3cdc495338b79856144e6951c
jjlin526 <jjlin526@gmail.com>
2022-09-27 17:24:46 -0500

    Update project mutation in GraphQL

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -144,6 +144,40 @@ const mutation = new GraphQLObjectType({
         return Project.findByIdAndRemove(args.id);
       },
     },
+    // Update a project
+    updateProject: {
+      type: ProjectType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            // Must be unique
+            name: "ProjectStatusUpdate",
+            values: {
+              new: { value: "Not Started" },
+              progress: { value: "In Progress" },
+              completed: { value: "Completed" },
+            },
+          }),
+        },
+      },
+      resolve(parent, args) {
+        return Project.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status,
+            },
+          },
+          // Create record if it does not exist
+          { new: true }
+        );
+      },
+    },
   },
 });
 
