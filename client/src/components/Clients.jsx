6a56813f6cfde0592d1a9a1764bbc86c6020be18
jjlin526 <jjlin526@gmail.com>
2022-09-28 13:58:10 -0500

    Apollo Client fetch clients data with GraphQL query

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,5 +1,21 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CLIENTS = gql`
+  query getClients {
+    clients {
+      id
+      name
+      email
+      phone
+    }
+  }
+`;
+
 export default function Clients() {
-  return (
-    <div>Clients</div>
-  )
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Something Went Wrong</p>;
+
+  return <>{!loading && !error && <h1>Clients</h1>}</>;
 }
