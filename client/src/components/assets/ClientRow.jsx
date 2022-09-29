a2581e0311f32458be891cf41a8d0c115917a52c
jjlin526 <jjlin526@gmail.com>
2022-09-28 19:02:51 -0500

    Refetch query after deleting a client to update UI

--- client/src/components/assets/ClientRow.jsx
+++ client/src/components/assets/ClientRow.jsx
@@ -1,13 +1,23 @@
 import { FaTrash } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../../mutations/clientMutations";
+import { GET_CLIENTS } from "../../queries/clientQueries";
 
 export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: {
+      id: client.id,
+    },
+    refetchQueries: [{ query: GET_CLIENTS }],
+  });
+
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
