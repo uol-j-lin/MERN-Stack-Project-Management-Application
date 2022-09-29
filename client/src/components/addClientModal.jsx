d961134479e0498fa859805cca11507d51529789
jjlin526 <jjlin526@gmail.com>
2022-09-29 12:22:22 -0500

    Add client logic in modal form, with UI change in real-time

--- client/src/components/addClientModal.jsx
+++ client/src/components/addClientModal.jsx
@@ -1,15 +1,37 @@
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 export default function AddClientModal() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
 
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    },
+  });
+
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(name, email, phone);
+    if (name === "" || email === "" || phone === "") {
+      return alert("Please fill in all fields");
+    }
+
+    addClient(name, email, phone);
+
+    setName("");
+    setEmail("");
+    setPhone("");
   };
 
   return (
