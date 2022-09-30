96733fd7509a8309a9f1e008fb27c5bbde698723
jjlin526 <jjlin526@gmail.com>
2022-09-29 19:05:36 -0500

    View button for projects

--- client/src/components/assets/ProjectCard.jsx
+++ client/src/components/assets/ProjectCard.jsx
@@ -6,6 +6,7 @@ export default function ProjectCard({ project }) {
           <div className="card-body">
             <div className="d-flex justify-content-between align-items-center"></div>
             <h5 className="card-title">{project.name}</h5>
+            <a className="btn btn-light" href={`/projects/${project.id}`}>View</a>
           </div>
         </div>
       </div>
