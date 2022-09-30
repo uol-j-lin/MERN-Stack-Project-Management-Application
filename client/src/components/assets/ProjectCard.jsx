132d8f20578698b5c3f07540fa1b2d7ee68e2e7c
jjlin526 <jjlin526@gmail.com>
2022-09-29 19:14:34 -0500

    Finish project cards with status

--- client/src/components/assets/ProjectCard.jsx
+++ client/src/components/assets/ProjectCard.jsx
@@ -6,8 +6,13 @@ export default function ProjectCard({ project }) {
           <div className="card-body">
             <div className="d-flex justify-content-between align-items-center"></div>
             <h5 className="card-title">{project.name}</h5>
-            <a className="btn btn-light" href={`/projects/${project.id}`}>View</a>
+            <a className="btn btn-light" href={`/projects/${project.id}`}>
+              View
+            </a>
           </div>
+          <p className="small">
+            Status: <strong>{project.status}</strong>
+          </p>
         </div>
       </div>
     </div>
