55e26365c5fcb9678930ce9547fb8101b50a7896
jjlin526 <jjlin526@gmail.com>
2022-09-30 13:31:51 -0500

    Query single project modal with client data

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/assets/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Project from "./pages/Project";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -37,6 +38,7 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/projects/:id" element={<Project />}></Route>
             <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </Router>
