d7b98b6c571166b98bc64338b2d02c30ce8d5f22
jjlin526 <jjlin526@gmail.com>
2022-10-01 15:08:39 -0500

    Get clients for select in add project form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Clients from "../components/assets/Clients";
 import Projects from "../components/assets/Projects";
 import AddClientModal from "../components/addClientModal";
+import AddProjectModal from "../components/addProjectModal";
 
 export default function Home() {
   return (
     <>
       <div className="d-flex gap-3 mb-4">
         <AddClientModal />
+        <AddProjectModal />
       </div>
       <Projects />
       <hr />
